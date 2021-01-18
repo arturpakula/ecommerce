bd5d488bb85b19ae720c82498426700e41161f5b
apakula <apakula@objectivity.co.uk>
2021-01-18 10:51:11 +0100

    Sign in component with standard credentials

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { singInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, singInWithGoogle } from '../../firebase/firebase.utils';
 import CustomButton from '../custom-button/custom-button.comonent';
 import FormInput from '../form-input/form-input.component';
 import './sign-in.styles.scss'
@@ -14,9 +14,15 @@ class SignIn extends Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({ email: '', password: '' });
+        const {email, password} = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email: '', password: ''});
+        } catch (error) {
+            alert(error);
+        }
     }
 
     handleChange = event => {
